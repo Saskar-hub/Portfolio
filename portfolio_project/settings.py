5d9c029a6be1cec17f75bb9ec79fbf3d585abab0
Saskar-hub <saskar616@gmail.com>
2026-04-01 19:50:11 +0545

    admin pannel error

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -142,3 +142,17 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # DEFAULT PK
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if os.environ.get("RENDER"):
+    try:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        if not User.objects.filter(username=os.environ.get("DJANGO_SUPERUSER_USERNAME")).exists():
+            User.objects.create_superuser(
+                username=os.environ.get("DJANGO_SUPERUSER_USERNAME"),
+                email=os.environ.get("DJANGO_SUPERUSER_EMAIL"),
+                password=os.environ.get("DJANGO_SUPERUSER_PASSWORD"),
+            )
+    except Exception as e:
+        print("Superuser creation skipped:", e)
